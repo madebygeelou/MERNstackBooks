1a6173ac93d324e48bf059ca30ea3252bd205a24
madebygeelou <125709909+madebygeelou@users.noreply.github.com>
2024-05-31 19:05:47 -0700

    finished server side

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ const app =express();
 
 app.use(express.json())
 
+app.use(cors( ));
+
 app.get('/', (request, response)=> {
     console.log(request)
     return response.status(234).send('Welcome!')
