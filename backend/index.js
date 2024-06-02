7a8aa50be6572f43a89a5487fd2d2ac5af1be4d9
Sunphetcharaj <153702282+Sunphetcharaj@users.noreply.github.com>
2024-06-02 14:56:10 +0000

    Fixed a lot, but gets error when making book

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import { PORT, mongoDBURL } from "./config.js"
 import mongoose from "mongoose";
 import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoutes.js';
+import cors from 'cors';
 
 const app =express();
 
--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -21,7 +21,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('http://localhost:5555/books', data)
+      .post('http://localhost:5173/books', data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created successfully', { variant: 'success' });
