a4a4463d1e4bd9101a49e710d5ba09ef2c221926
Sunphetcharaj <153702282+Sunphetcharaj@users.noreply.github.com>
2024-06-02 01:57:45 +0000

    Started setting up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import CreateBooks from './pages/CreateBooks';
+import ViewBook from './pages/ViewBook';
+import EditBooks from './pages/EditBooks';
+import RemoveBook from './pages/RemoveBook';
+
+ const App = () => {
+   return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBooks />} />
+      <Route path='/books/details/:id' element={<ViewBook />} />
+      <Route path='/books/edit/:id' element={<EditBooks />} />
+      <Route path='/books/delete/:id' element={<RemoveBook />} />
+    </Routes>
+   )
+ }
+ 
+ export default App
